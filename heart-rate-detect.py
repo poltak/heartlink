43fc9ae85db86db238cf3e13a8072f692fb8f4ac
Cisco Local Admin <localadmin@cisco-dim.(none)>
2015-03-17 18:16:34 -0700

    Added hardware logic

--- heart-rate-detect.py
+++ heart-rate-detect.py
@@ -1,4 +1,6 @@
 import re
+import usb.core
+import time
 
 RESTING_HEART_RATE_RANGE = [40,100]
 ERRATIC_HEART_RATE_RANGE = [100,160]
@@ -8,7 +10,34 @@ def getDataString():
     Gets the current data string from the sensor.
     TODO
     """
-    pass
+    os.environ["USBIP_SERVER"]='10.10.31.170'
+    dev = usb.core.find(idVendor=0x0403, idProduct=0x6001) # Polar usb heart rate monitor
+    if dev is None:
+        return ValueError('Device not found')
+
+    # begin generic usb init
+    dev.set_configuration()
+    print usb.util.get_string(dev, 256, 1)
+    print usb.util.get_string(dev, 256, 2)
+    print usb.util.get_string(dev, 256, 3)
+    # end generic usb init
+
+    # begin ftdi init
+    dev.ctrl_transfer(0x40, 0x3, 0x1a, 0x0)
+    dev.ctrl_transfer(0x40, 0x1, 0x202, 0x0)
+    dev.ctrl_transfer(0x40, 0x4, 0x8, 0x0)
+    dev.ctrl_transfer(0x40, 0x2, 0x0, 0x0)
+    dev.ctrl_transfer(0x40, 0x1, 0x100, 0x0)
+    dev.ctrl_transfer(0x40, 0x6, 0x11a, 0x0)
+    dev.write(2,'\r')
+    # end ftdi init
+
+    dev.write(2, 'G1\r')
+    a = dev.read(1,64)
+    print a.tostring()
+    return a.tostring()
+    time.sleep(5)
+    #pass
 
 
 def parseDataString(dataString):
